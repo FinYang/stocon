e32d24b108faa4039be89364c79a6802b0b37853
Fin <Fin.Yang@monash.edu>
2019-05-05 21:50:53 +1000

    update label to plot

--- zoom_plot.R
+++ zoom_plot.R
@@ -1,32 +1,21 @@
 
 library(ggforce)
-
+library(tidyverse)
 
 
 pm <- readRDS("em_single_asset.rds")
+upb <- 70
 v <- sapply(pm, function(x) x[[2]])
 v0 <- 3350.08
+v <- v[1:upb]
+data <- data.frame(x=seq_along(v), y=v/1000, labels = NA)
+data2 <- data.frame(x= rep(length(v)+1, 2), y=c(3.55, 2.9), labels = c("PA", "EM"), vzoom = T)
 
-library(tidyverse)
-data <- data.frame(x=seq_along(v), y=v/1000, vzoom = T)
 ggplot(data, aes(y=y, x=x))+
   geom_line() +
   labs(x = "Number of Iterations",
        y = expression(paste("Value at time 0, ", V[0], "('000)"))) +
-  geom_hline(yintercept = v0/1000, color = "blue") +
-  geom_label(data = data.frame(x= c(100, 100), y=c(3.7, 2.7), label = c("PA", "EM"), vzoom = T),
-            mapping = aes(x=x, y=y, label = label)) +
-  facet_zoom(x = x >75, ylim = c(2,5), horizontal = F)
-
-ggplot(mapping = aes(y=v/1000, x=seq_along(v)))+
-  geom_line(show.legend = T) +
-  labs(x = "Number of Iterations",
-       y = expression(paste("Value at time 0, ", V[0], "('000)"))) +
-  geom_hline(yintercept = v0/1000, color = "blue") +
-  # geom_label(data = data.frame(x= c(100, 100), y=c(3.7, 2.7), label = c("PA", "EM"), vzoom = T),
-            # mapping = aes(x=x, y=y, label = label)) +
-  facet_zoom(x = x >75, ylim = c(2,5), horizontal = F, zoom.data = vzoom) +
-  geom_label_repel(data = data.frame(x= c(100, 100), y=c(3.7, 2.7), label = c("PA", "EM"), vzoom = T), aes(label = labels))
-
-
-
+  facet_zoom(x = x >3, ylim = c(2,5),horizontal = F,  zoom.data = vzoom) +
+  geom_label(data = data2,
+            mapping = aes(label = labels))+
+  geom_hline(mapping = aes(yintercept = v0/1000), color = "blue")
